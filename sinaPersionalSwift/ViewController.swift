0e45032b46700713e809b8cef23eb535384bf22f
tc1993 <tangchao@cloud4magic.com>
2017-06-15 09:39:05 +0800

    Initial Commit

---
+++ sinaPersionalSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sinaPersionalSwift
+//
+//  Created by 唐超 on 6/15/17.
+//  Copyright © 2017 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
